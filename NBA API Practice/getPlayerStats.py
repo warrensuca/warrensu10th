52a35917c816d3959cbd37f26cae4c11e03e0a28
Warren Su <warrensuca@gmail.com>
2025-09-09 00:15:37 -0700

    start creating the player class

--- NBA API Practice/getPlayerStats.py
+++ NBA API Practice/getPlayerStats.py
@@ -20,12 +20,38 @@ df = statsSeason.get_data_frames()[0]
 
 
 davis = playercareerstats.PlayerCareerStats(player_id="203076")
-print(davis.get_normalized_dict())
+print(davis.get_normalized_dict().keys())
 print('\n')
 print(davis.get_normalized_dict()['SeasonTotalsRegularSeason'][0])
+print(davis.get_normalized_dict()['CareerTotalsRegularSeason'][0])
 davisDict = davis.get_normalized_dict()
 avgPoints = 0
 seasons = davisDict['SeasonTotalsRegularSeason']
 for i in range(len(seasons)):
-    avgPoints += seasons[i]['PTS']
+    avgPoints += seasons[i]['PTS']/seasons[i]['GP']
 print(avgPoints/len(seasons))
+
+class Player:
+    def __init__(self, id):
+        self.id = id
+        self.playerstats = playercareerstats.PlayerCareerStats(player_id="203076")
+        self.regularSeasons = self.playerstats.get_normalized_dict()['SeasonTotalsRegularSeason']
+        self.totals = self.playerstats.get_normalized_dict()['CareerTotalsRegularSeason']
+
+    def getPPG():
+        
+        return (self.totals['PTS']/self.totals['GP'])
+        
+    def getRPG():
+        return (self.totals['REB']/self.totals['GP'])
+    
+    def getAST():
+        return (self.totals['AST']/self.totals['GP'])
+    
+    def getSTL():
+        avgSTL = 0
+        for i in range(len(seasons)):
+            avgSTL += seasons[i]['STL']/seasons[i]['GP']
+        return (self.totals['AST']/self.totals['GP'])
+    
+    
