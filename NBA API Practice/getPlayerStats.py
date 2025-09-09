2e3259ba1e7d09c98c8d4d7448acc025f308a02d
Warren Su <warrensu@xwPod.local>
2025-09-09 09:58:24 -0700

    finish basic player class

--- NBA API Practice/getPlayerStats.py
+++ NBA API Practice/getPlayerStats.py
@@ -2,7 +2,7 @@ from nba_api.stats.static import players
 from nba_api.stats.endpoints import playercareerstats
 
 from nba_api.stats.endpoints import leaguedashplayerstats
-
+from nba_api.stats.endpoints import boxscoreadvancedv2
 
 # get_players returns a list of dictionaries, each representing a player.
 nba_players = players.get_active_players()
@@ -31,6 +31,9 @@ for i in range(len(seasons)):
     avgPoints += seasons[i]['PTS']/seasons[i]['GP']
 print(avgPoints/len(seasons))
 
+davisAdv = boxscoreadvancedv2.BoxScoreAdvancedV2(player_id = "203076")
+davisAdvDict = davisAdv.get_dict()
+print(davisAdvDict)
 class Player:
     def __init__(self, id):
         self.id = id
@@ -38,20 +41,35 @@ class Player:
         self.regularSeasons = self.playerstats.get_normalized_dict()['SeasonTotalsRegularSeason']
         self.totals = self.playerstats.get_normalized_dict()['CareerTotalsRegularSeason']
 
-    def getPPG():
+
+    def getName(self):
+        for i in range(len(nba_players)):
+            if nba_players[i]['id'] == self.id:
+                return nba_players[i]['full_name']
+        
+    
+    def getimageURL(self):
+        return "https://cdn.nba.com/headshots/nba/latest/1040x760/{}.png".format(self.id)
+
+    def getPPG(self):
         
         return (self.totals['PTS']/self.totals['GP'])
         
-    def getRPG():
+    def getRPG(self):
         return (self.totals['REB']/self.totals['GP'])
     
-    def getAST():
+    def getAST(self):
         return (self.totals['AST']/self.totals['GP'])
     
-    def getSTL():
+    def getSTL(self):
         avgSTL = 0
         for i in range(len(seasons)):
             avgSTL += seasons[i]['STL']/seasons[i]['GP']
         return (self.totals['AST']/self.totals['GP'])
     
+    def getPCT(self):
+        return (self.totals['FG_PCT'])
+    
+    def get3PCT(self):
+        return (self.totals['FG3_PCT'])
     
